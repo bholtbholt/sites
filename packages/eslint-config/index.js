8d423389a3ee5ed7e9161da63844ca47397a1775
Brian Holt <bholt@digitalocean.com>
2025-03-15 17:48:39 -0700

    Add eslint-config

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,39 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+const gitignorePath = fileURLToPath(new URL(`${process.env.ROOT}/.gitignore`, import.meta.url));
+const svelteConfig = fileURLToPath(new URL(`${process.env.CWD}/svelte.config.js`, import.meta.url));
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    ignores: ['eslint.config.js', 'svelte.config.js'],
+
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  },
+);
