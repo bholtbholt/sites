98cc6bd7ffe5049b9789e011c1d260a282ef24c4
Brian Holt <bholt@digitalocean.com>
2025-03-16 13:37:16 -0700

    Initial commit comma comma

---
+++ packages/commacomma/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="text-center">
+  <h1 class="mb-8 text-8xl font-black">Comma Comma</h1>
+  <p class="text-2xl">
+    Music by <a class="underline" href="mailto:bholtbholt@icloud.com">Brian Holt</a> coming soon.
+    <a
+      class="underline"
+      href="https://distrokid.com/hyperfollow/commacomma1/ce-soir--la-mer"
+      target="_blank">Presave on Spotify</a
+    >
+  </p>
+  <p></p>
+</div>
