5a90dcfd7663ff4b71dd5412af1c0c46ec314670
Brian Holt <bholtbholt@icloud.com>
2025-05-01 11:02:45 -0700

    Remove email address (#10)
    * Update +page.svelte
    
    * Update +page.svelte

--- packages/badbadpower/src/routes/+page.svelte
+++ packages/badbadpower/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="text-center">
 	<h1 class="mb-8 text-8xl font-black">Bad Bad Power</h1>
 	<p class="text-2xl">
-		Music by <a class="underline" href="mailto:bholtbholt@icloud.com">Brian Holt</a> coming soon.
+		Music by Brian Holt coming soon.
 		<a
 			class="underline"
 			href="https://distrokid.com/hyperfollow/badbadpower/what-could-possibly-go-wrong"
--- packages/commacomma/src/routes/+page.svelte
+++ packages/commacomma/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="text-center">
 	<h1 class="mb-8 text-8xl font-black">Comma Comma</h1>
 	<p class="text-2xl">
-		Music by <a class="underline" href="mailto:bholtbholt@icloud.com">Brian Holt</a> coming soon.
+		Music by Brian Holt coming soon.
 		<a
 			class="underline"
 			href="https://distrokid.com/hyperfollow/commacomma1/ce-soir--la-mer"
