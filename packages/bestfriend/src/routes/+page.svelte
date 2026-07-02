0510f6367aedc27ba93bc084c9e2101d5cfbb906
Brian Holt <bholtbholt@icloud.com>
2026-07-02 13:16:50 -0700

    Bf release (#69)
    * Add best-friend entry
    
    * Link to app store
    
    * Prettier

--- packages/bestfriend/src/routes/+page.svelte
+++ packages/bestfriend/src/routes/+page.svelte
@@ -9,13 +9,34 @@
 </svelte:head>
 
 <div class="space-y-8 my-18">
-	<h1 class="text-5xl sm:text-9xl mb-3 sm:mb-8 font-bold tracking-tight text-orange-500 dark:text-orange-400">
+	<h1
+		class="text-5xl sm:text-9xl mb-3 sm:mb-8 font-bold tracking-tight text-orange-500 dark:text-orange-400"
+	>
 		{site.name}
 	</h1>
 
 	<p class="text-lg sm:text-3xl leading-relaxed text-stone-600 dark:text-stone-300">
-		{tagline} Follow the development Substack for releases, updates, and tips!
+		{tagline}
+		<a
+			class="block my-2"
+			href="https://apps.apple.com/app/best-friend/id6782250723"
+			rel="external noopener noreferrer"
+			target="_blank"
+		>
+			<img src="/AppStore.svg" alt="Apple App Store logo" class="inline" />
+		</a>
 	</p>
 
-	<iframe src="https://epbf.substack.com/embed?transparent=1" width="480" height="150" style="border: 0; background: transparent" frameborder="0" scrolling="no"></iframe>
+	<p class="text-stone-600 dark:text-stone-300 mb-0">
+		Follow the development Substack for releases, updates, and tips!
+	</p>
+
+	<iframe
+		src="https://epbf.substack.com/embed?transparent=1"
+		width="360"
+		height="150"
+		style="border: 0; background: transparent"
+		frameborder="0"
+		scrolling="no"
+	></iframe>
 </div>
