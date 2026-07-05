494b367ed169b80e55a06b0e9b448c3773b102f9
Brian Holt <bholtbholt@icloud.com>
2026-07-04 22:40:14 -0700

    Update portfolio description

--- packages/portfolio/src/lib/index.ts
+++ packages/portfolio/src/lib/index.ts
@@ -1,4 +1,4 @@
-export const siteIntro = `I'm a senior software engineer with more than a decade of experience in JavaScript and Ruby. I like working on frontend architecture and developer experience. After hours you can find me playing guitar or drums, designing logos and posters, or gobbling up the menus from the newest restaurants.`;
+export const siteIntro = `I'm a staff software engineer with more than 15 years of experience in JavaScript and Ruby. I like working on frontend architecture and developer experience. After hours you can find me playing guitar or drums, designing logos and posters, or gobbling up the menus from the newest restaurants.`;
 
 export { default as Button } from './Button.svelte';
 export { default as InputText } from './InputText.svelte';
--- packages/portfolio/src/routes/now/+page.svelte
+++ packages/portfolio/src/routes/now/+page.svelte
@@ -7,23 +7,19 @@
 >
 	<h1 class="mb-0! text-emerald-800 dark:text-cyan-400">What I'm up now</h1>
 	<p>
-		After being away for several years, my partner and I moved back to Vancouver, Canada to
-		reconnect with friends, family, and our communities.
-	</p>
-	<p>
-		I'm regularly releasing music under
+		My partner and I live in Vancouver, Canada with our cat, Pekoe. I regularly release music under
 		<a href="https://www.commacomma.ca" target="_blank">Comma Comma</a> and
-		<a href="https://www.badbadpower.com" target="_blank">Bad Bad Power</a>. You can find me making
-		beats at
+		<a href="https://www.badbadpower.com" target="_blank">Bad Bad Power</a> (but I really need to
+		build those sites). You can find me making beats live at
 		<a href="https://lossapostacos.com/" target="_blank" rel="noreferrer">
 			Los Sapos Tacos Tuesday Jam</a
 		>, monthly at
 		<a href="https://www.instagram.com/loopsessionsvancouver/" target="_blank" rel="noreferrer">
 			Vancouver Loop Sessions</a
 		>, or periodically on
-		<a href="https://www.tiktok.com/@commacomma" target="_blank" rel="noreferrer">TikTok</a>. I'm
-		also learning classical piano at
-		<a href="https://vsoschoolofmusic.ca" target="_blank" rel="noreferrer">VSO School</a>.
+		<a href="https://www.instagram.com/badbadpower" target="_blank" rel="noreferrer">Instagram</a>
+		and
+		<a href="https://www.tiktok.com/@commacomma" target="_blank" rel="noreferrer">TikTok</a>.
 	</p>
 	<p>
 		I work at <a href="https://www.digitalocean.com" target="_blank">DigitalOcean</a> on the UI Platform
@@ -31,7 +27,13 @@
 		tools to make product development easier for frontend engineers.
 	</p>
 	<p>
+		I just released my first iOS App, <a
+			href="https://apps.apple.com/app/best-friend/id6782250723"
+			target="_blank">Best Friend</a
+		>, a companion app for the Teenage Engineering K.O. II and am super proud of it. Check it out!
+	</p>
+	<p>
 		This <a href="https://nownownow.com/about" target="_blank">now page</a> was inspired by
-		<a href="https://sivers.org/now" target="_blank">Derek Sivers</a>. Last updated April 2025
+		<a href="https://sivers.org/now" target="_blank">Derek Sivers</a>. Last updated July 2026
 	</p>
 </article>
