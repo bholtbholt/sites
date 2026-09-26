8f8d36eb15f63a3f433487a2667cfa206d478909
Brian Holt <bholtbholt@icloud.com>
2026-09-25 19:55:46 -0700

    Revert "Fix Svelte workspace typecheck resolution"
    This reverts commit 22ead17acdb135f2e403335a3c83249f0d4f94d5.

--- packages/portfolio/src/app.d.ts
+++ packages/portfolio/src/app.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite/client" />
-
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 import type { MarkdownFiles } from '$lib/types';
